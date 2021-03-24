95c957a2d729b8f520d520ed418e051078a307dd
NoahRix <rix.noah@yahoo.com>
2021-03-24 16:36:40 -0500

    for testing

---
+++ backend/query_testing.sql
@@ -0,0 +1,20 @@
+use `h1gtfw3uuuyktywj`;
+insert into `inventory` values
+(150, 0),
+(151, 0),
+(152, 0),
+(153, 0),
+(154, 0),
+(155, 0);
+
+use `h1gtfw3uuuyktywj`;
+delete from `inventory`
+
+use `h1gtfw3uuuyktywj`;
+select * from `inventory`;
+
+use `h1gtfw3uuuyktywj`;
+delete from `inventory` where `part_id` in (150, 151, 152, 153, 154, 155);
+
+use `h1gtfw3uuuyktywj`;
+delete from `inventory` where `part_id` > 70;
