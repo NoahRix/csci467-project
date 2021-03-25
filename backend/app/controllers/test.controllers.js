df08ea5a88691a0845e98c9a2346a4aeb29aff42
FlyingPorkChop <marcus.schumach@gmail.com>
2021-03-25 17:11:11 -0500

    think im done with my routs section

--- backend/app/controllers/test.controllers.js
+++ backend/app/controllers/test.controllers.js
@@ -1,25 +1,5 @@
 const test = require("../models/test.models.js");
 
-/*
-let html = "";
-data.forEach(item => {
-    html += `<img src="${item.pictureURL}"><br>\n` 
-});
-res.send(html);
-*/
-/*
-let html = "";
-        data.forEach(item => {
-            html += `<div>`
-            html += `<p>number: ${item.number}</p>`;
-            html += `<p>description: ${item.description}</p>`;
-            html += `<p>price: ${item.price}</p>`;
-            html += `<p>weight: ${item.weight}</p>`;
-            html += `<p>pictureURL: ${item.pictureURL}</p>`;
-            html += `</div>`
-        });
-*/
-
 exports.parts = (req, res) => {
     test.parts(data => {
         res.send(data);
