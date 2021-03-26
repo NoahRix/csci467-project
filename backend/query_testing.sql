4747f7e1a5531c92ef32ac8e35a64e7797f597d5
NoahRix <rix.noah@yahoo.com>
2021-03-25 21:25:52 -0500

    More testing stuff.

--- backend/query_testing.sql
+++ backend/query_testing.sql
@@ -5,7 +5,10 @@ insert into `inventory` values
 (152, 0),
 (153, 0),
 (154, 0),
-(155, 0);
+(155, 0),
+(156, 0),
+(157, 0),
+(158, 0);
 
 use `h1gtfw3uuuyktywj`;
 delete from `inventory`
@@ -18,3 +21,9 @@ delete from `inventory` where `part_id` in (150, 151, 152, 153, 154, 155);
 
 use `h1gtfw3uuuyktywj`;
 delete from `inventory` where `part_id` > 70;
+
+SELECT BookCode, Title, CONCAT_WS(', ', AuthorLast, AuthorFirst) AS AuthorName, Price 
+FROM Book INNER JOIN Wrote 
+USING (BookCode) JOIN Author 
+USING (AuthorNum) 
+WHERE Title = 'Black House' AND Sequence = 1;
