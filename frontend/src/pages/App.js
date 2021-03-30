3e96fe17d9e2f8a729fb6c9239efe34bb9928db6
NoahRix <rix.noah@yahoo.com>
2021-03-30 18:15:53 -0500

    Added routes for the initial pages

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,13 +1,27 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CustomerOrders from "./CustomerOrders";
 import Home from './Home';
+import InventoryDashboard from "./InventoryDashboard";
+import OrdersDashboard from "./OrdersDashboard";
+import ShippingDashboard from "./ShippingDashboard";
+import ShoppingCart from "./ShoppingCart";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar/>
         <Switch>
           <Route exact path="/" component={Home}/>
+          <Route exact path="/CustomerOrders" component={CustomerOrders}/>
+          <Route exact path="/InventoryDashboard" component={InventoryDashboard}/>
+          <Route exact path="/OrdersDashboard" component={OrdersDashboard}/>
+          <Route exact path="/ShippingDashboard" component={ShippingDashboard}/>
+          <Route exact path="/ShoppingCart" component={ShoppingCart}/>
         </Switch>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
