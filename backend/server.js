c72a56504ec7982b0a6e0e0a8e9e890130e617d9
NoahRix <rix.noah@yahoo.com>
2021-03-31 18:54:18 -0500

    stuff

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.options('*', cors()); 
 
 app.get("/", (req, res) => {
     res.send("Hello");
