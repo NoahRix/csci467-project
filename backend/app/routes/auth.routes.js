efe3e101c97c3d044c00361f8666d0632aa53715
NoahRix <rix.noah@yahoo.com>
2021-03-31 11:49:16 -0500

    Added custoemr login, logout, and customer logout functions

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -2,6 +2,7 @@ module.exports = app => {
     const auth = require("../controllers/auth.controllers.js");
     
     app.post('/api/auth/customer-login', auth.customerLogin);
+    app.post('/api/auth/employee-login', auth.employeeLogin);
     //app.post('/api/post/auth/register', auth.register);
     //app.post('/api/post/auth/logout', auth.logout);
     //app.post('/api/post/auth/token', auth.token);
