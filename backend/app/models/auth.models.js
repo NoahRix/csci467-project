bcff73bbe457f62393478fcd80ac390c34b141e0
NoahRix <rix.noah@yahoo.com>
2021-03-31 11:48:50 -0500

    Added custoemr login, logout, and customer logout functions

--- backend/app/models/auth.models.js
+++ backend/app/models/auth.models.js
@@ -1,11 +1,41 @@
 const db_tools = require("../utils/db-tools");
 
 /**
- *  Gets the customers password based on the email.
+ *  Gets the customer's password based on the id.
  * 
- *  @param email Customer's email string.
+ *  @param id Customer'sid number.
  *  @param result Callback to send back data.
  */
- exports.getCustomerPassword = (email, result) => {
-    db_tools.execute('select password from customers where email = ?', result, [email]);
+ exports.getCustomerPassword = (id, result) => {
+    db_tools.execute('select password from customers where id = ?', result, [id]);
+}
+
+/**
+ *  Gets the employee's password based on the id.
+ * 
+ *  @param id Employee's id number.
+ *  @param result Callback to send back data.
+ */
+ exports.getEmployeePasswordAndRole = (id, result) => {
+    db_tools.execute('select password, is_admin from workers where id = ?', result, [id]);
+}
+
+/**
+ *  Expires the access token of the customer to be logged out.
+ *
+ *  @param id Customer's id number.
+ *  @param result Callback function to send the response data.
+ */
+ exports.logout = (id, result) => {
+    dbt.execute("update customers set refresh_token = NULL where id = ?", result, [id]);
+}
+
+/**
+ *  Expires the access token of the employee to be logged out.
+ *
+ *  @param id Employee's id number.
+ *  @param result Callback function to send the response data.
+ */
+ exports.logout = (id, result) => {
+    dbt.execute("update workers set refresh_token = NULL where id = ?", result, [id]);
 }
