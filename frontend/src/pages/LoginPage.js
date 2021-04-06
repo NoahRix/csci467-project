f6551e6bd8e7240c7ad91cffee81c58dafe9fda8
NoahRix <rix.noah@yahoo.com>
2021-04-06 17:47:13 -0500

    Added option to login as customer or employee.

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,16 @@
-import React, {useContext, useState} from 'react';
-import { Button, TextField, Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios';
-import { AuthContext } from '../utils/AuthContext';
+import React, { useContext, useState } from "react";
+import {
+    Button,
+    TextField,
+    Paper,
+    Checkbox,
+    FormControlLabel,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import { AuthContext } from "../utils/AuthContext";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     mainBody: {
         width: "100%",
         display: "flex",
@@ -14,46 +20,75 @@ const useStyles = makeStyles((theme) => ({
         width: "30%",
     },
 }));
-  
-export default function LoginPage(){
 
+export default function LoginPage(props) {
     const classes = useStyles();
 
     const [IdInput, setIdInput] = useState("");
     const [passwordInput, setPasswordInput] = useState("");
     const [showError, setShowError] = useState(false);
+    const [isEmployee, setIsEmployee] = useState(false);
 
-    const { setIsCustomerAuthed, setId } = useContext(AuthContext);
+    const { setIsAdmin, setIsCustomerAuthed, setIsEmployeeAuthed, setId } = useContext(
+        AuthContext
+    );
 
     const handleLogin = () => {
         axios({
-            method: 'post',
-            url: 'http://localhost:3001/api/auth/user-login',
-            data: {id: IdInput, password: passwordInput, is_customer: true}
-        }).then(res => {
+            method: "post",
+            url: "http://localhost:3001/api/auth/user-login",
+            data: {
+                id: IdInput,
+                password: passwordInput,
+                is_customer: !isEmployee,
+            },
+        }).then((res) => {
             console.log(res.data);
-            if(res.data.is_authed)
-                setId(parseInt(IdInput))
-            setIsCustomerAuthed(res.data.is_authed);
+            if (res.data.is_authed) {
+                setId(parseInt(IdInput));
+                if (isEmployee) setIsEmployeeAuthed(isEmployee);
+                else setIsCustomerAuthed(!isEmployee);
+                setIsAdmin(res.data.is_admin);
+                props.history.push("/");
+            } else setShowError(true);
         });
-    }
+    };
 
-    return(
+    return (
         <div className={classes.mainBody}>
             <form className={classes.formBody}>
-                <Paper style={{display: "flex", flexDirection: "column"}}>
+                <Paper style={{ display: "flex", flexDirection: "column" }}>
                     <TextField
-                        onBlur={e => {setIdInput(e.target.value)}}
+                        style={{ width: "200px", margin: "auto" }}
+                        label="User"
+                        error={showError}
+                        onBlur={(e) => {
+                            setIdInput(e.target.value);
+                        }}
                     />
                     <TextField
+                        style={{ width: "200px", margin: "auto" }}
+                        label="Password"
                         type="password"
-                        onBlur={e => {setPasswordInput(e.target.value)}}
-                     />
-                    <Button
-                        onClick={handleLogin}
-                    >
-                        Login
-                    </Button>
+                        error={showError}
+                        onBlur={(e) => {
+                            setPasswordInput(e.target.value);
+                        }}
+                    />
+                    <FormControlLabel
+                        style={{ margin: "auto" }}
+                        control={
+                            <Checkbox
+                                onChange={(e) =>
+                                    setIsEmployee(e.target.checked)
+                                }
+                                color="default"
+                            />
+                        }
+                        label="Employee?"
+                        labelPlacement="start"
+                    />
+                    <Button onClick={handleLogin}>Login</Button>
                 </Paper>
             </form>
         </div>
