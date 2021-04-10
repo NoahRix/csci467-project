15d7b327ae7de9c51267a977cec5b92e004ee6b2
NoahRix <rix.noah@yahoo.com>
2021-04-10 11:59:42 -0500

    added option to use local database.

--- backend/app/utils/db.js
+++ backend/app/utils/db.js
@@ -1,9 +1,11 @@
+let aws = false;
+
 //Database configuration.
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-    host: 'pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'lk29l9vskr8x2w7e',
-    password: 'kbi6sgrvps0wiorm',
+    host: aws ? 'pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' : 'localhost',
+    user: aws ? 'lk29l9vskr8x2w7e' : 'root',
+    password: aws ? 'kbi6sgrvps0wiorm' : 'password',
     database: 'h1gtfw3uuuyktywj',
     port: 3306,
     multipleStatements: true
