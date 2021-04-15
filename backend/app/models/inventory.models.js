aeaf2f4cb1d54682779cbc446cb8ec8d93c093b4
NoahRix <rix.noah@yahoo.com>
2021-04-15 14:42:05 -0500

    Added update rows and get by part id APIs.

--- backend/app/models/inventory.models.js
+++ backend/app/models/inventory.models.js
@@ -25,8 +25,36 @@ exports.byPartID = (part_id, result) => {
  *  @param result Callback to send back data.
  *  @param inventory_row_object {quantity: (Number), part_id, (Number)}
  */
-exports.update = (inventory_row_object, result) => {
+ exports.update = (inventory_row_object, result) => {
     db_tools.execute(`update inventory set quantity = ? where part_id = ?`, result, Object.values(inventory_row_object));
+}
+
+/**
+ *  Updates multiple existing rows.
+ * 
+ *  @param result Callback to send back data.
+ *  @param inventory_row_array [{quantity: (Number), part_id, (Number)}]
+ */
+ exports.updateRows = (inventory_row_array, result) => {
+
+    let sql_statement = "update inventory set quantity = case part_id \n";
+    let placholders = "";
+    let case_values = [];
+    let placeholder_values = [];
+
+    inventory_row_array.forEach(row => {
+        sql_statement += "when ? then ? \n"
+        placholders += "?,";
+        case_values.push(row.part_id);
+        case_values.push(row.quantity);
+        placeholder_values.push(row.part_id);
+    });
+
+    sql_statement += `end\n where part_id in (${placholders.replace(/,$/, "")})`; 
+
+    console.log(sql_statement);
+
+    db_tools.execute(sql_statement, result, case_values.concat(placeholder_values));
 }
 
 /**
@@ -66,4 +94,21 @@ exports.deleteRowsByIDs = (part_ids, result) => {
     });
 
     db_tools.execute(`delete from inventory where part_id in (` + placholders.replace(/, $/, " ") + `)`, result, part_ids);
+}
+
+/**
+ *  This gets rows based on a list of part ids.
+ * 
+ *  @param part_ids Array of part ids for where-in clause.
+ *  @param result Callback to send back data.
+ */
+ exports.getRowsByIDs = (part_ids, result) => {
+    console.log("part_ids: " + part_ids)
+    let placholders = '';
+
+    part_ids.forEach(() => {
+        placholders += '?, ';
+    });
+
+    db_tools.execute(`select * from inventory where part_id in (` + placholders.replace(/, $/, " ") + `)`, result, part_ids);
 }
